c81ecdfb13b44adb1f23c676b123f3a1ea761ee0
d0iasm <doiasami1219@gmail.com>
2017-12-30 16:45:28 +0900

    Update init function

--- data-sets-and-queries/range-query/range_search.cpp
+++ data-sets-and-queries/range-query/range_search.cpp
@@ -1,37 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-struct Node {
-  int l, r;
-  // node *l, *r;
-};
-
-struct Point {
-  int id, x, y;
-};
-
-class KdTree {
-private:
-  Node* root;
-  int* tree;
-public:
-  int size;
-  KdTree (int n) {
-    
-  }
-  void insert(int l, int r, int depth);
-  vector<int> query();
-};
-
-void KdTree::insert(int l, int r, int depth) {
-
-}
-
+const int ST_SIZE = (1 << 18) - 1;
+const int MAX_N = 500001;
+const int MAX_Q = 20001;
+const int MAX = 1000000001;
 int n, q;
 int size;
-Point* p;
-Node* tree;
+int a[MAX_N];
+int i[MAX], j[MAX], k[MAX];
+
+int nums[MAX_N];
+vector<int> data[ST_SIZE];
+
+
+void init(int k, int l, int r) {
+  if (r - l == 1) {
+    data[k].push_back(a[l]);
+  } else {
+    int lch = k * 2 + 1;
+    int rch = k * 2 + 2;
+    init(lch, l, (l + r) / 2);
+    init(rch, (l + r) / 2, r);
+    data[k].resize(r - l);
+
+    merge(data[lch].begin(), data[lch].end(), data[rch].begin(), data[rch].end(),
+          data[k].begin());
+  }
+}
 
 int main() {
   ios::sync_with_stdio(false);
